dbcc0cefef4d2d47b1e1c6796eeb877148b34951
Cleidson Oliveira <dev.cleidson@gmail.com>
2022-02-03 00:04:10 -0300

    refactor: usign app and logger factory

--- src/main.mjs
+++ src/main.mjs
@@ -1,22 +1,13 @@
 import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import { routes } from "./routes.mjs";
 
-const app = express();
-app.use(express.json());
+import { loggerFactory } from "./factories/loggerFactory.mjs";
+import { appFactory } from "./factories/appFactory.mjs";
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-app.use(helmet());
-
-app.use(routes);
+const logger = loggerFactory();
+const app = appFactory(logger);
 
 const port = process.env.PORT || 5009;
+
 app.listen(port, () => {
-  console.log(`Aplicativo iniciado na porta ${port}`);
+  logger.info(`Aplicativo iniciado na porta ${port}`);
 });
--- src/routes.mjs
+++ src/routes.mjs
@@ -1,28 +1,62 @@
 import { Router } from "express";
 import { LoginService } from "./services/loginService.mjs";
 import { userValidator } from "./validators/userValidator.mjs";
+import { v4 } from "uuid";
 
-const routes = Router();
+const routesFactory = (logger) => {
+  const routes = Router();
 
-const loginService = new LoginService({
-  execute: userValidator,
-});
+  const loginService = new LoginService(
+    {
+      execute: userValidator,
+    },
+    logger
+  );
 
-routes.post("/provasyscoin", async (req, res) => {
-  const data = req.query;
-  const result = await loginService.login(data);
+  const loggerMiddleware = async (req, res, next) => {
+    const _idRequest = v4();
+    req._id = _idRequest;
+    logger.info(`${_idRequest} - Iniciando requisição`);
+    req.logger = logger;
 
-  return res.status(result.success ? 200 : 400).json(result);
-});
+    res.on("finish", () => {
+      if (res.statusCode >= 400) {
+        req.logger.error(
+          `${_idRequest} - Requisição Finalizada ${res.statusCode}`
+        );
+      } else
+        req.logger.info(
+          `${_idRequest} - Requisição Finalizada ${res.statusCode}`
+        );
+    });
 
-routes.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Hello World",
+    await next();
+  };
+
+  routes.use(loggerMiddleware);
+
+  routes.post("/provasyscoin", async (req, res) => {
+    const id = req._id;
+    logger.info(`${id} - [POST] /provasyscoin`);
+    const data = req.query;
+    data._idRequest = id;
+
+    const result = await loginService.login(data);
+
+    return res.status(result.success ? 200 : 400).json(result);
   });
-});
 
-routes.get("*", (req, res) => {
-  return res.status(404).json({ message: "Página não encontrada" });
-});
+  routes.get("/", (req, res) => {
+    res.status(200).json({
+      message: "Hello World",
+    });
+  });
 
-export { routes };
+  routes.get("*", (req, res) => {
+    return res.status(404).json({ message: "Página não encontrada" });
+  });
+
+  return routes;
+};
+
+export { routesFactory as routes };
