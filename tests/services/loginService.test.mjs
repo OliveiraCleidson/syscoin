58613290a52a4204d1c20de95cb6fa397514d40f
Cleidson Oliveira <dev.cleidson@gmail.com>
2022-02-02 22:35:07 -0300

    test: ensure login service feature

--- tests/services/loginService.test.mjs
+++ tests/services/loginService.test.mjs
@@ -1,27 +1,32 @@
 import { LoginService } from "../../src/services/loginService.mjs";
+import jest from "jest-mock";
 
 describe("LoginService", () => {
   // System Under Test
   let sut;
   let sutRequest;
+  let validator;
   beforeEach(() => {
-    sut = new LoginService();
+    validator = {
+      execute: jest.fn(() => Promise.resolve({ success: true })),
+    };
+    sut = new LoginService(validator);
     sutRequest = {
       user: "syscoin",
       password: "meEscolhe",
     };
   });
 
-  it("should be logged if user is syscoin and password meEscolhe", () => {
-    const result = sut.login(sutRequest);
+  it("should be logged if user is syscoin and password meEscolhe", async () => {
+    const result = await sut.login(sutRequest);
 
     expect(result).toMatchObject({
       success: true,
     });
   });
 
-  it("should be send message if the login was success", () => {
-    const result = sut.login(sutRequest);
+  it("should be send message if the login was success", async () => {
+    const result = await sut.login(sutRequest);
 
     expect(result).toMatchObject({
       success: true,
@@ -31,8 +36,8 @@ describe("LoginService", () => {
 
   for (let i = 0; i < 3; i++) {
     const user = Math.random().toString(36).substring(7);
-    it(`should be success false if user not syscoin #${i}`, () => {
-      const result = sut.login({ ...sutRequest, user });
+    it(`should be success false if user not syscoin #${i}`, async () => {
+      const result = await sut.login({ ...sutRequest, user });
 
       expect(result).toMatchObject({
         success: false,
@@ -42,8 +47,8 @@ describe("LoginService", () => {
 
   for (let i = 0; i < 3; i++) {
     const password = Math.random().toString(36).substring(7);
-    it(`should be success false if password is not correct #${i}`, () => {
-      const result = sut.login({ ...sutRequest, password });
+    it(`should be success false if password is not correct #${i}`, async () => {
+      const result = await sut.login({ ...sutRequest, password });
 
       expect(result).toMatchObject({
         success: false,
@@ -51,9 +56,9 @@ describe("LoginService", () => {
     });
   }
 
-  it("should be send message if user is not correct", () => {
+  it("should be send message if user is not correct", async () => {
     const user = Math.random().toString(36).substring(7);
-    const result = sut.login({ ...sutRequest, user });
+    const result = await sut.login({ ...sutRequest, user });
 
     expect(result).toMatchObject({
       success: false,
@@ -61,13 +66,31 @@ describe("LoginService", () => {
     });
   });
 
-  it("should be send message if password is not correct", () => {
+  it("should be send message if password is not correct", async () => {
     const password = Math.random().toString(36).substring(7);
-    const result = sut.login({ ...sutRequest, password });
+    const result = await sut.login({ ...sutRequest, password });
 
     expect(result).toMatchObject({
       success: false,
       message: "Credenciais erradas.",
+    });
+  });
+
+  it("should be call validator", async () => {
+    await sut.login(sutRequest);
+
+    expect(validator.execute.mock.calls.length).toBe(1);
+  });
+
+  it("should return validator result if success is false", async () => {
+    validator.execute = jest.fn(() =>
+      Promise.resolve({ success: false, flag: true })
+    );
+    const result = await sut.login(sutRequest);
+
+    expect(result).toMatchObject({
+      success: false,
+      flag: true,
     });
   });
 });
