937ada206ae91a945e1179357c3472bb52c81b54
Cleidson Oliveira <dev.cleidson@gmail.com>
2022-02-03 00:03:25 -0300

    feat: added logger to login service

--- src/services/loginService.mjs
+++ src/services/loginService.mjs
@@ -1,22 +1,32 @@
 import { getUser } from "../data/dataContext.mjs";
 
 export class LoginService {
-  constructor(validator) {
+  constructor(validator, logger) {
     this.validator = validator;
+    this.logger = logger;
   }
 
   async login(data) {
     const isValidResponse = await this.validator.execute(data);
 
-    if (!isValidResponse.success) return isValidResponse;
+    if (!isValidResponse.success) {
+      this.logger.info(`${data._idRequest} - LoginService dados inválidos`);
+      return isValidResponse;
+    }
 
     const user = getUser(data.user);
 
-    if (!user || data.password !== user.password)
+    if (!user || data.password !== user.password) {
+      this.logger.info(
+        `${data._idRequest} - LoginService usuário ou senha incorretos`
+      );
+
       return {
         success: false,
         message: "Credenciais erradas.",
       };
+    }
+    this.logger.info(`${data._idRequest} - LoginService dados válidos`);
 
     return {
       success: true,
